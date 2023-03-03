3313f2656aa359e157172723672bfd6ec6ca9d18
GAudrey <gilmantaudrey.ag@gmail.com>
2023-03-03 10:48:13 +0100

    Vue.js - providers

---
+++ 10-providers/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import Header from "./ui/Header.vue";
+import { ref } from "vue";
+
+const isLogged = ref(false);
+const toggleConnect = () => {
+  isLogged.value = !isLogged.value;
+};
+</script>
+
+<template>
+  <div class="bg-slate-50 flex flex-col min-h-screen">
+    <Header />
+    <main class="container mx-auto m-8 border-2 border-dashed rounded p-8">
+      <div class="justify-around flex">
+        <button
+          class="btn primary"
+          type="button"
+          @click="toggleConnect"
+          v-if="!isLogged"
+        >
+          Se connecter
+        </button>
+        <button class="btn cancel" type="button" @click="toggleConnect" v-else>
+          Se déconnecter
+        </button>
+      </div>
+    </main>
+  </div>
+</template>
---
+++ 10-providers/src/ui/Header.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TopMenu from "./TopMenu.vue";
+</script>
+<template>
+  <header
+    class="mt-6 mx-8 px-8 shadow-md rounded-md flex justify-between items-center bg-white overflow-hidden"
+  >
+    <img src="/logo.png" alt="logo" class="w-16 animation-image" />
+    <TopMenu />
+  </header>
+</template>
