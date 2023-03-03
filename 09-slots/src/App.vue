02792c8bd99711e5939790922beaebe160ed9b61
GAudrey <gilmantaudrey.ag@gmail.com>
2023-03-03 09:59:27 +0100

    Vue.js - slots end

--- 09-slots/src/App.vue
+++ 09-slots/src/App.vue
@@ -1,5 +1,30 @@
 <script setup>
+import { ref } from "vue";
 import Quote from "./components/Quote.vue";
+import CardFormation from "./components/CardFormation.vue";
+
+const formations = ref([
+  {
+    id: 1,
+    name: "Développeur affinity designer",
+    imgSrc:
+      "https://api.technocite.be/static/sm/1673420263606-Affinity-Photo.png",
+    description: `<h2 class="text-xl">Devenez développeur de contenu pour des contenus non-autorisés</h2>`,
+  },
+  {
+    id: 2,
+    name: "Développeur gaming",
+    imgSrc: "https://api.technocite.be/static/sm/1644313419821-gaming5.png",
+    description: `<p class="text-red-600">Jouer en classe au lieu d'apprendre</p>`,
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    imgSrc:
+      "https://api.technocite.be/static/sm/1645093105204-javascript%20(2).png",
+    description: `<p>Ca sert à rien. De toute façon, y a pas de boulot dans le JS.</p>`,
+  },
+]);
 </script>
 
 <template>
@@ -10,11 +35,26 @@ import Quote from "./components/Quote.vue";
       <img src="/logo.png" alt="logo" class="w-16 animation-image" />
     </header>
     <main class="container mx-auto m-8 border-2 border-dashed rounded p-8">
-      <Quote
-        ><div class="font-bold text-lg">
-          "Deux tartines de confiture équivaut à une Frambush"
-        </div></Quote
-      >
+      <Quote>
+        <div class="font-bold text-lg">
+          "Deux tartines de confiture équivalent à une Frambush"
+        </div>
+      </Quote>
+      <div class="grid grid-cols-2 gap-8 w-[1000px] mx-auto">
+        <CardFormation
+          v-for="formation in formations"
+          :key="formation.id"
+          :imgSrc="formation.imgSrc"
+          :name="formation.name"
+        >
+          <template v-slot:header>
+            <h2 class="title primary">{{ formation.name }}</h2>
+          </template>
+          <template v-slot:description>
+            <div v-html="formation.description"></div>
+          </template>
+        </CardFormation>
+      </div>
     </main>
   </div>
 </template>
--- 09-slots/src/components/CardFormation.vue
+++ 09-slots/src/components/CardFormation.vue
@@ -11,5 +11,12 @@ defineProps(["imgSrc", "name"]);
       :alt="name"
       class="w-full h-64 rounded-t-md object-cover"
     />
+    <div class="px-4 pb-16 pt-6 flex flex-col flex-grow">
+      <header><slot name="header"></slot></header>
+      <div><slot name="description"></slot></div>
+      <div class="mt-4 flex-grow flex items-end">
+        <button class="btn primary">S'inscrire</button>
+      </div>
+    </div>
   </div>
 </template>
